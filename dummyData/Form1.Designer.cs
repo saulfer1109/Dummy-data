f7f9c8d9e5993b1361eea957c1fff2bef825efa6
saul <saul.espinoza.rivera@gmail.com>
2024-05-17 20:16:50 -0700

    Agregados archivos de generación de datos y exportación

---
+++ dummyData/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace dummyData
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            listView1 = new ListView();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 76);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(12, 121);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(12, 161);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(121, 23);
+            comboBox3.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(170, 24);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(12, 208);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(776, 230);
+            listView1.TabIndex = 5;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(170, 161);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 6;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(listView1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox3);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private TextBox textBox1;
+        private Button button1;
+        private ListView listView1;
+        private Button button2;
+    }
+}
---
+++ dummyData/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace dummyData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cargar datos de origen
+            DatosAlumno datosAlumno = GeneradorDatos.CargarDatosOrigen();
+
+            // Solicitar cantidad de alumnos
+            int cantidadAlumnos = GeneradorDatos.SolicitarCantidadAlumnos();
+
+            // Generar lista de alumnos
+            List<Alumno> listaAlumnos = GeneradorDatos.GenerarListaAlumnos(datosAlumno, cantidadAlumnos);
+
+            // Mostrar la lista de alumnos en la consola
+            GeneradorDatos.MostrarListaAlumnos(listaAlumnos);
+
+            // Permitir exportar la lista (opcional)
+            GeneradorDatos.ExportarListaAlumnos(listaAlumnos);
+        }
+    }
+
+    public class Alumno
+    {
+        public int Expediente { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Correo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        public Alumno(int expediente, string nombreCompleto, string correo, DateTime fechaNacimiento)
+        {
+            Expediente = expediente;
+            NombreCompleto = nombreCompleto;
+            Correo = correo;
+            FechaNacimiento = fechaNacimiento;
+        }
+    }
+
+    public class DatosAlumno
+    {
+        public string[] NombresRusos { get; set; }
+        public string[] ApellidosRusos { get; set; }
+        public string[] NombresEspanol { get; set; }
+        public string[] ApellidosEspanol { get; set; }
+        public string[] NombresChino { get; set; }
+        public string[] ApellidosChino { get; set; }
+        public string[] NombresFrances { get; set; }
+        public string[] ApellidosFrances { get; set; }
+    }
+
+    public static class GeneradorDatos
+    {
+        private static Random random = new Random();
+
+        public static string GenerarNombreAleatorio(DatosAlumno datosAlumno, string origen)
+        {
+            string[] nombres;
+            string[] apellidos;
+
+            switch (origen)
+            {
+                case "Ruso":
+                    nombres = datosAlumno.NombresRusos;
+                    apellidos = datosAlumno.ApellidosRusos;
+                    break;
+                case "Español":
+                    nombres = datosAlumno.NombresEspanol;
+                    apellidos = datosAlumno.ApellidosEspanol;
+                    break;
+                case "Chino":
+                    nombres = datosAlumno.NombresChino;
+                    apellidos = datosAlumno.ApellidosChino;
+                    break;
+                case "Francés":
+                    nombres = datosAlumno.NombresFrances;
+                    apellidos = datosAlumno.ApellidosFrances;
+                    break;
+                default:
+                    throw new Exception("Origen no válido: " + origen);
+            }
+
+            int indiceNombreAleatorio = random.Next(nombres.Length);
+            int indiceApellidoAleatorio = random.Next(apellidos.Length);
+
+            return nombres[indiceNombreAleatorio] + " " + apellidos[indiceApellidoAleatorio];
+        }
+
+        public static string GenerarCorreoElectronico(int expediente)
+        {
+            return expediente + "@unison.mx";
+        }
+
+        public static DateTime GenerarFechaNacimientoAleatoria()
+        {
+            DateTime fechaMinima = new DateTime(1998, 1, 1);
+            DateTime fechaMaxima = new DateTime(2004, 12, 31);
+
+            int diasAleatorios = random.Next((int)(fechaMaxima - fechaMinima).TotalDays);
+            DateTime fechaNacimiento = fechaMinima.AddDays(diasAleatorios);
+
+            return fechaNacimiento;
+        }
+
+        public static List<Alumno> GenerarListaAlumnos(DatosAlumno datosAlumno, int cantidadAlumnos)
+        {
+            List<Alumno> listaAlumnos = new List<Alumno>();
+
+            for (int i = 0; i < cantidadAlumnos; i++)
+            {
+                string origen = SeleccionarOrigenAleatorio();
+                string nombreCompleto = GenerarNombreAleatorio(datosAlumno, origen);
+                string correo = GenerarCorreoElectronico(i + 1);
+                DateTime fechaNacimiento = GenerarFechaNacimientoAleatoria();
+
+                Alumno nuevoAlumno = new Alumno(i + 1, nombreCompleto, correo, fechaNacimiento);
+                listaAlumnos.Add(nuevoAlumno);
+            }
+
+            return listaAlumnos;
+        }
+
+        private static string SeleccionarOrigenAleatorio()
+        {
+            string[] posiblesOrigenes = { "Ruso", "Español", "Chino", "Francés" };
+            int indiceOrigenAleatorio = random.Next(posiblesOrigenes.Length);
+            return posiblesOrigenes[indiceOrigenAleatorio];
+        }
+
+        public static DatosAlumno CargarDatosOrigen()
+        {
+            DatosAlumno datosAlumno = new DatosAlumno();
+
+            datosAlumno.NombresRusos = new string[] { "Ivan", "Dmitri", "Alexei", "Natalia", "Irina" };
+            datosAlumno.ApellidosRusos = new string[] { "Ivanov", "Petrov", "Smirnov", "Volkova", "Kuznetsova" };
+
+            datosAlumno.NombresEspanol = new string[] { "José", "María", "Antonio", "Ana", "Carlos" };
+            datosAlumno.ApellidosEspanol = new string[] { "García", "López", "Martínez", "González", "Rodríguez" };
+
+            datosAlumno.NombresChino = new string[] { "李", "王", "张", "刘" };
+            datosAlumno.ApellidosChino = new string[] { "陈", "李", "王", "张", "赵" };
+
+            datosAlumno.NombresFrances = new string[] { "Jean", "Marie", "Pierre", "Anne", "Nicolas" };
+            datosAlumno.ApellidosFrances = new string[] { "Martin", "Bernard", "Dubois", "Durand", "Leroy" };
+
+            return datosAlumno;
+        }
+
+        public static int SolicitarCantidadAlumnos()
+        {
+            Console.WriteLine("¿Cuántos alumnos desea generar?");
+            int cantidadAlumnos;
+
+            while (!int.TryParse(Console.ReadLine(), out cantidadAlumnos) || cantidadAlumnos <= 0)
+            {
+                Console.WriteLine("Ingrese un número positivo de alumnos:");
+            }
+
+            return cantidadAlumnos;
+        }
+
+        public static void MostrarListaAlumnos(List<Alumno> listaAlumnos)
+        {
+            Console.WriteLine("\nLista de Alumnos Generados:");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("{0,-15} {1,-25} {2,-30} {3}", "Expediente", "Nombre Completo", "Correo", "Fecha Nac.");
+            Console.WriteLine("------------------------------------");
+
+            foreach (Alumno alumno in listaAlumnos)
+            {
+                Console.WriteLine("{0,-15} {1,-25} {2,-30} {3}", alumno.Expediente, alumno.NombreCompleto, alumno.Correo, alumno.FechaNacimiento.ToString("yyyy/MM/dd"));
+            }
+
+            Console.WriteLine("\n------------------------------------");
+        }
+
+        public static void ExportarListaAlumnos(List<Alumno> listaAlumnos)
+        {
+            Console.WriteLine("\nExportación a CSV no implementada aún.");
+        }
+    }
+}
+
